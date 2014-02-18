f9c33d35132f5dcffcd95b8643006ebc27a80584
Russell John-Baptiste <luckvery@gmial.com>
2014-02-18 11:05:44 -0500

    add size option to gravatar helper, test for error messages and flash css, refactor flash related view code

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -27,14 +27,24 @@ describe "User pages" do
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
+
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
       end
     end
 
     describe "with valid information" do
+      let(:new_user) { "Example User" }
+
       before do
-        fill_in "Name",         with: "Example User"
+        fill_in "Name",         with: new_user
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
@@ -43,6 +53,22 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title(new_user) }
+        it { should have_content("Welcome to the Sample App!") }
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+
     end
 
   end
